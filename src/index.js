26a354c39ecbc1b536fd71cc9489ce84e7605caf
Viktoriia Ksenofontova <glory_viktory@hotmail.com>
2021-01-11 18:04:35 +0200

    внесение изменений

--- src/index.js
+++ src/index.js
@@ -1,46 +1,40 @@
 import './styles.css';
 
-import toastr from 'toastr';
-import options from './js/toastr.options';
-import 'toastr/build/toastr.min.css';
-toastr.options = options;
-import debounce from 'lodash.debounce';
-
 import apiService from './js/apiService';
 import refs from './js/refs';
 import updateImageMarkup from './js/update-images-markup';
 
 
 refs.searchForm.addEventListener('submit', event => {
-   event.preventDefault();
+  event.preventDefault();
+  
   const form = event.currentTarget;
-  
   apiService.query = form.elements.query.value;
- 
-   refs.gallery.innerHTML = "";
-   form.reset();
-    apiService.resetPage();
+  refs.gallery.innerHTML = "";
+  apiService.resetPage();
   refs.loadMoreBtn.classList.add('is-hidden');
-  
-  apiService.fetchImages().then(images => {
-    updateImageMarkup(images);
-    refs.loadMoreBtn.classList.remove('is-hidden');
-  });
-
-});
-
-refs.loadMoreBtn.addEventListener('click', () => {
-  apiService.fetchImages().then(images => {
-    updateImageMarkup(images);
-
-    window.scrollBy({
-  top: 100,
-  left: 0,
-  behavior: 'smooth'
-});
-     window.scrollBy(0, window.innerHeight);
-  });
+    
+  apiService.fetchImages()
+    .then(images => {
+        updateImageMarkup(images);
+        refs.loadMoreBtn.classList.remove('is-hidden'); 
+    })
   
 });
+
+
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
  
+
+function onLoadMoreBtnClick() {
+  apiService.fetchImages().then(images => {
+  updateImageMarkup(images);
+  
+  window.scrollBy({
+  top: window.innerHeight,
+  left: 0,
+  behavior: 'smooth'});
+  });
+}
+
 
